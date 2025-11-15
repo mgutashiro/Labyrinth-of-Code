eb2c2b9261c357c9a43997c6e50bf484816e8d82
Monica Aichouri <69013706+mgu0625@users.noreply.github.com>
2025-11-15 16:47:57 -0500

    lesson 42

---
+++ NodeAndThreeJS_Projects/Lesson42/src/shaders/wobble/fragment.glsl
@@ -0,0 +1,25 @@
+uniform vec3 uColorA;
+uniform vec3 uColorB;
+
+// varying vec2 vUv;
+varying float vWobble;
+
+void main()
+{
+    float colorMix = smoothstep(- 1.0, 1.0, vWobble);
+    csm_DiffuseColor.rgb = mix(uColorA, uColorB, colorMix);
+
+    // Mirror step
+    csm_Metalness = step(0.25, vWobble);
+    csm_Roughness = 1.0 - csm_Metalness;
+
+    // Shinny tip
+    csm_Roughness = 1.0 - colorMix;
+
+    // csm_FragColor.rgb = vec3(1.0, 0.5, 0.5);
+    // csm_DiffuseColor.rgb = vec3(1.0, 0.5, 0.5);
+    // csm_DiffuseColor.rgb = vec3(vUv, 0.5);
+    // csm_Metalness = step(0.0, sin(vUv.x * 100.0 + 0.5));
+    // csm_Roughness = 1.0 - csm_Metalness;
+    // csm_FragColor.rgb = vec3(vWobble);
+}
